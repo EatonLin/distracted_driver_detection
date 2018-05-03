41a5d2b5c78671381377f8d0a4d013b73dc46a54
flwas <flwas595@gmail.com>
2018-05-04 07:55:14 +0800

    Update test.py

--- test.py
+++ test.py
@@ -14,26 +14,21 @@ driver_classname_count = 0
 drivers_count = 0
 
 
-def add_driver_classname_count(classname, count):
-    dict = {}
-    dict[classname] = count
-    return dict
+def add_driver_subject_information(dict, subject, classname, count):
+    if subject in dict:
+        dict[subject].append(count)
+    else:
+        dict[subject] = [count]
 
-
-def add_driver_subject_information(subject_dict, subject, class_dict):
-    subject_dict[subject] = class_dict
 
 for index, row in data.iterrows():
     if subject_temp is not row["subject"]:
         if driver_classname_count > 1:
-            print("subject:{} classname:{} count:{}".format(subject_temp, classname_temp, driver_classname_count))
-            classname_dict = add_driver_classname_count(classname_temp, driver_classname_count)
-            add_driver_subject_information(subject_dict, subject_temp, classname_dict)
+            add_driver_subject_information(subject_dict, subject_temp, classname_temp, driver_classname_count)
             driver_classname_count = 0
 
-        if driver_count != 0:
-            print("subject:{} count:{}".format(subject_temp, driver_count))
-            # add_driver_subject_information(subject_dict, subject_temp, classname_dict)
+        # if driver_count != 0:
+        #     add_driver_subject_information(subject_dict, subject_temp, classname_temp, driver_classname_count)
         subject_temp = row["subject"]
         driver_count = 1
         drivers_count += 1
@@ -41,14 +36,29 @@ for index, row in data.iterrows():
         driver_count += 1
         if classname_temp is not row["classname"]:
             if driver_classname_count != 0:
-                print("subject:{} classname:{} count:{}".format(subject_temp, classname_temp, driver_classname_count))
+                add_driver_subject_information(subject_dict, subject_temp, classname_temp, driver_classname_count)
             classname_temp = row["classname"]
             driver_classname_count = 1
         else:
             driver_classname_count += 1
 
+subject_dict[subject_temp].append(driver_classname_count)
+new_dict = [(key, subject_dict[key]) for key in sorted(subject_dict.keys())]
 
+import numpy as np
+import matplotlib.pyplot as plt
 
+n = 10
+x = np.arange(n)
+fig, ax = plt.subplots()
+bar_width = 0.3
+opacity = 0.4
+index = 0
 
+color_list = ['r', 'b', 'c', 'g', 'k', 'm', 'y', 'r', 'b', 'c']
 
-print("Drivers dict:", subject_dict)
+for item in new_dict:
+    plt.bar(x + index * bar_width, item[1], bar_width, alpha=opacity, color='r', label=item[0])
+
+plt.legend()
+plt.show()
